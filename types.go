f4cbae2f78e3af6e1b3e1084ba2d69b96cf24fcb
Payam Madjidi <madjidi@kth.se>
2019-09-15 08:35:45 +0200

    refactoring..

--- types.go
+++ types.go
@@ -5,24 +5,6 @@ import "github.com/gorilla/mux"
 type leaderboard map[string]int
 type groups map[string]map[string]bool
 
-const EMPTYSTRING = ""
-const NOTFOUND = -1
-const TIMEOUT = 2
-
-// potentially 100.000 open http requests, OBS buffered channels
-// Obs buffered channels create risk for data loss on server crash...
-// Tune for low latency on external http requests
-
-const MAXQUEUELENGTH = 100000
-
-const MAXITERATIONLIMIT = 1000 // concurrent request to API server
-
-// always good to put a bound on datastructures...
-
-const MAXNUMBEROFSTEPSINPUT = 1000
-const MAXNUMBERSOFWALKERS = 1000000
-const MAXNUMBEROFGROUPS = 100000
-const MAXNUMBEROFWALKERSINGROUP = 2000
 
 type pedometers struct {
 	name string
